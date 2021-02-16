be80efb59a4c4fa8609dc4ab84a05d134edaaad0
alex k <alex.kuhn1123@gmail.com>
2021-02-16 11:21:54 -0800

    initial commit

---
+++ javaFundamentals/HelloWorld.java
@@ -0,0 +1,12 @@
+public class HelloWorld { //class name must exactly match file name
+    public static void main(String[] args){ //starting point for any Java program
+        System.out.println("Hello World");
+    }
+}
+
+// to compile:
+// in terminal, run --> javac FileName.java
+//      this will compile program & create a .class file containing bytecode
+
+// to run:
+// in terminal, run --> java FileName.java
